bc315e4b124ae385c1f346daa2d691d97be40762
Benson Che <bche@resdat.com>
2014-01-04 23:14:24 -0900

    Fix prefs

--- src/com/bensonche/batteryalert/MainActivity.java
+++ src/com/bensonche/batteryalert/MainActivity.java
@@ -3,16 +3,60 @@ package com.bensonche.batteryalert;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.ToggleButton;
 
 public class MainActivity extends Activity {
+	private AdapterView.OnItemSelectedListener SpinnerListener = new AdapterView.OnItemSelectedListener() {
+		public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+			SharedPreferences pref = MainActivity.this.getPreferences(MODE_PRIVATE);
+			SharedPreferences.Editor editor = pref.edit();
+			
+			String prefName = "";
+			
+			switch(parent.getId()) {
+				case R.id.frequencySpinner:
+					prefName = "frequency";
+					break;
+				case R.id.levelSpinner:
+					prefName = "batteryLevel";
+					break;
+			}
+			
+			editor.putInt(prefName, position);
+			
+			editor.commit();
+		}
+		
+		public void onNothingSelected(AdapterView<?> parent) {
+			
+		}
+	};
+	
+	private TextWatcher EditTextListener = new TextWatcher() {
+		public void afterTextChanged(Editable s) {
+			SharedPreferences pref = MainActivity.this.getPreferences(MODE_PRIVATE);
+			SharedPreferences.Editor editor = pref.edit();
+			
+			editor.putString("email", s.toString());
+			
+			editor.commit();
+		}
+
+		public void beforeTextChanged(CharSequence s, int start, int count,	int after) {}
+
+		public void onTextChanged(CharSequence s, int start, int before, int count) {}
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,29 +70,32 @@ public class MainActivity extends Activity {
 		for (int i = 5; i <= 50; i += 5)
 			lvlList.add(i);
 
-		ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(this,
-				android.R.layout.simple_spinner_item, lvlList);
+		ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, lvlList);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		levelSpinner.setAdapter(adapter);
+		levelSpinner.setOnItemSelectedListener(SpinnerListener);
 
 		// Populate frequencySpinner
 		Spinner frequencySpinner = (Spinner) findViewById(R.id.frequencySpinner);
-
-		ArrayAdapter<CharSequence> freqAdapter = ArrayAdapter
-				.createFromResource(this, R.array.frequencyArray,
+		ArrayAdapter<CharSequence> freqAdapter = ArrayAdapter.createFromResource(this, R.array.frequencyArray,
 						android.R.layout.simple_spinner_item);
-		freqAdapter
-				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		freqAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		frequencySpinner.setAdapter(freqAdapter);
-
-		SharedPreferences pref = this.getPreferences(MODE_PRIVATE);
+		frequencySpinner.setOnItemSelectedListener(SpinnerListener);
+		
+		// Set Listener
+		EditText txtEmailAddress = (EditText) findViewById(R.id.txtEmailAddress);
+		txtEmailAddress.addTextChangedListener(EditTextListener);
 
 		// Set preferences
-		EditText txtEmailAddress = (EditText) findViewById(R.id.txtEmailAddress);
+		SharedPreferences pref = this.getPreferences(MODE_PRIVATE);
+		
+		ToggleButton onOffToggle = (ToggleButton) findViewById(R.id.OnOffToggle);
 
 		levelSpinner.setSelection(pref.getInt("batteryLevel", 3));
 		frequencySpinner.setSelection(pref.getInt("frequency", 2));
 		txtEmailAddress.setText(pref.getString("email", ""));
+		onOffToggle.setChecked(pref.getBoolean("OnOff", false));
 	}
 
 	@Override
@@ -59,13 +106,12 @@ public class MainActivity extends Activity {
 	}
 
 	public void onOffClicked(View view) {
-		EditText txtEmailAddress = (EditText) findViewById(R.id.txtEmailAddress);
-
-		if (((ToggleButton) view).isChecked())
-			txtEmailAddress.setText("on");
-		else
-			txtEmailAddress.setText("off");
-
+		SharedPreferences pref = this.getPreferences(MODE_PRIVATE);
+		SharedPreferences.Editor editor = pref.edit();
+		
+		boolean checked =  ((ToggleButton) view).isChecked();
+		editor.putBoolean("OnOff", checked);
+		
+		editor.commit();
 	}
-
 }
