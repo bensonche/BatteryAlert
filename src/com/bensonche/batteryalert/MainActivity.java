610ecc45a6622a5ade8032c67db2f99bb7fcff09
Benson Che <bche@resdat.com>
2014-01-04 21:28:28 -0900

    Add on/off toggle

--- src/com/bensonche/batteryalert/MainActivity.java
+++ src/com/bensonche/batteryalert/MainActivity.java
@@ -1,8 +1,11 @@
 package com.bensonche.batteryalert;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ToggleButton;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,14 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		EditText txtBatteryLevel = (EditText)findViewById(R.id.txtBattLevel);
+		EditText txtFrequency =(EditText)findViewById(R.id.txtFrequency);
+		EditText txtEmailAddress =(EditText)findViewById(R.id.txtEmailAddress);
+		
+		txtBatteryLevel.setText("D");
+		txtFrequency.setText("B");
+		txtEmailAddress.setText("abc@gmail.com");
 	}
 
 	@Override
@@ -17,6 +28,17 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void onOffClicked(View view)
+	{
+		EditText txtBatteryLevel = (EditText)findViewById(R.id.txtBattLevel);
+		
+		if(((ToggleButton) view).isChecked())
+			txtBatteryLevel.setText("on");
+		else
+			txtBatteryLevel.setText("off");
+
 	}
 
 }
